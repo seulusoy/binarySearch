d179c9b3af73e2ba3b292f500a39d8049061ee49
seulusoy <ulusoy.coding@gmail.com>
2024-02-03 16:36:23 +0300

    First codes without border check
    binarySearchLeft and binarySearchRight are coded but check for array boundaries are missing

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -8,34 +8,83 @@ using namespace std;
 
 #define V 1 //verbose prints intermediate values for start and end
 
-vector<int> numbers{0, 0, 2, 2, 2, 2, 6, 6, 6, 6, 9};
+vector<int> numbers{0, 0, 3, 3, 3, 3, 8, 8, 8, 8, 11};
 
-void binarySearch2Left(){
-
+void binarySearchLeft(int check){
+	int start = 0;
+	int end = numbers.size()-1;
+	int mid;
+	while(start<=end){
+		mid = (start+end)/2;
+		if (V==1){
+			cout << "check: " << check;
+			cout << " start: " << start;
+			cout << " end: " << end;
+			cout << " mid: " << mid;
+			cout << " num: " << numbers[mid];
+			cout << endl;
+		}
+		if(numbers[mid]<check){
+			start = mid+1;
+		}
+		else{
+			end = mid-1;
+		}
+	}
+	mid = start;
+	cout << mid << " " << numbers[mid] << endl;
 }
 
-void binarySearch2Right(){
+void binarySearchRight(int check){
+	int start = 0;
+	int end = numbers.size()-1;
+	int mid;
+	while(start<=end){
+		mid = (start+end)/2;
+		if (V==1){
+			cout << "check: " << check;
+			cout << " start: " << start;
+			cout << " end: " << end;
+			cout << " mid: " << mid;
+			cout << " num: " << numbers[mid];
+			cout << endl;
+		}
+		if(numbers[mid]>check){
+			end = mid-1;
+		}
+		else{
+			start = mid+1;
+		}
+	}
+	mid = end;
+	cout << mid << " " << numbers[mid] << endl;
 }
 
-void binarySearch4Lower(){
+
+void binarySearchLower(int check){
 }
 
-void binarySearch4Upper(){
+
+void binarySearchUpper(int check){
 }
 
 int main(){
-	binarySearch2Left();
-	binarySearch2Right();
-	binarySearch4Lower();
-	binarySearch4Upper();
+	binarySearchLeft(3);
+	binarySearchRight(3);
+	binarySearchLeft(4);
+	binarySearchRight(4);
+	
+	
+	binarySearchLower(4);
+	binarySearchUpper(4);
 	
 	cout << endl;
 	
 	vector<int>::iterator it;
 	
-	it = lower_bound(numbers.begin(), numbers.end(), 2);
+	it = lower_bound(numbers.begin(), numbers.end(), 3);
 	cout << "2 with lower bound " << (it-numbers.begin()) << endl;
-	it = upper_bound(numbers.begin(), numbers.end(), 2);
+	it = upper_bound(numbers.begin(), numbers.end(), 3);
 	cout << "2 with upper bound " << (it-numbers.begin()) << endl;
 	it = lower_bound(numbers.begin(), numbers.end(), 4);
 	cout << "4 with lower bound " << (it-numbers.begin()) << endl;
